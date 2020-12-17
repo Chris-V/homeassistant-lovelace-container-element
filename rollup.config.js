f003c6cbddad8cecfe5ba041ef79e292afa7d578
Christopher Viel <viel.christopher@gmail.com>
2020-12-17 14:32:56 -0500

    Brand new custom:container-element for use in picture-element cards

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import json from '@rollup/plugin-json';
+
+const dev = process.env.ROLLUP_WATCH;
+
+export default [
+  {
+    input: 'src/container-element.js',
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [
+      nodeResolve({}),
+      commonjs(),
+      json(),
+      babel({ exclude: 'node_modules/**' }),
+      !dev && terser(),
+      dev && serve({
+        contentBase: ['./dist'],
+        host: '0.0.0.0',
+        port: 5000,
+        allowCrossOrigin: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }),
+    ],
+  },
+];
---
+++ src/container-element.js
@@ -0,0 +1,95 @@
+import { createThing } from 'custom-card-helpers';
+
+customElements.define(
+  "container-element",
+  class HuiContainerElement extends HTMLElement {
+    constructor() {
+      super();
+
+      this._hass = undefined;
+      this._config = {};
+
+      this._container = undefined;
+      this._elements = [];
+    }
+
+    setConfig(config) {
+      if (!Array.isArray(config.elements)) {
+        throw new Error("Elements are required.");
+      }
+
+      this._config = config;
+
+      this.updateContainerStyle();
+
+      this.clearChildren();
+      this._elements = this.createChildren(this._config.elements);
+      this.refreshChildren();
+    }
+
+    updateContainerStyle() {
+      this.removeAttribute("style");
+      this.style.transform = "none";
+
+      this.applyStyleProperties(this, this._config.style || {});
+    }
+
+    set hass(hass) {
+      this._hass = hass;
+
+      this.refreshChildren();
+    }
+
+    clearChildren() {
+      this._elements.forEach((element) => {
+        if (element.parentElement) {
+          element.parentElement.removeChild(element);
+        }
+      });
+
+      this._elements = [];
+    }
+
+    createChildren(elements) {
+      return this._config.elements
+        .map((config) => this.createChild(config));
+    }
+
+    createChild(config) {
+      if (typeof config !== "object" || !config.type) {
+        throw Error('Invalid element. Missing "type" property.')
+      }
+
+      const type = config.type.indexOf("custom:") === 0
+        ? config.type
+        : `custom:hui-${config.type}-element`
+
+      const element = createThing({ ...config, type });
+      element.classList.add("element");
+
+      this.applyStyleProperties(element, config.style || {})
+
+      return element;
+    }
+
+    applyStyleProperties(node, style) {
+      Object.entries(style).forEach(([property, value]) => {
+        node.style.setProperty(property, value);
+      });
+    }
+
+    refreshChildren() {
+      if (!this._hass) {
+        return;
+      }
+
+      this._elements.forEach((element) => {
+        element.hass = this._hass;
+
+        if (!element.parentElement) {
+          this.appendChild(element);
+        }
+      });
+    }
+  }
+);
